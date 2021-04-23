6bb6710fec2bd9dcd7279cbe094194c8eecda0c1
Pablo Quesada <pablo.quesada@gsquad.com>
2021-04-23 16:34:59 -0300

    adding new file

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+nombre = input('Ingrese nombre: ')
+apellido = input('Ingrese apellido: ')
+edad = int(input('Ingrese edad: '))
+#condicion_edad = str(condicion_edad)
+
+if edad < 18:
+    #print('menor')
+    condicion_edad = 'menor'
+elif (edad >= 18 and edad < 65):
+    condicion_edad = 'mayor'
+elif edad >= 65 and edad < 120:
+    condicion_edad = 'jubiliado'
+else:
+    condicion_edad = 'cadaver'
+
+print('Su nombre es: '+nombre+' '+apellido+' '+'Y usted es '+condicion_edad)
