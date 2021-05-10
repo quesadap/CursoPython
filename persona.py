e7e7bb1d98f81fd787de049592d28b8ba8c64ac3
Pablo Quesada <pablo.quesada@gsquad.com>
2021-05-10 16:03:50 -0300

    adding new things

---
+++ persona.py
@@ -0,0 +1,35 @@
+#crear una clase persona
+#nombre
+#apellido
+#edad
+#dni
+#METODOS:
+#nombrar > CArlos > Carlos
+#dar apellido > suarez > Suarez
+#cumplir > cumplir años
+#gustos de ropa > 0 a 9 le gusta la moda infantil
+# 10 a 18 le gusta la moda juvenil
+# 19 a 50 le gusta la ropa casual
+# mas de 50 le gusta la ropa de viejo
+
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        #self.apellido = apellido
+        #self .edad = 0
+        #self.dni = dni
+
+    def nombrar(self, mi_nombre):
+        try:
+            mi_nombre = self.nombre
+            #return (mi_nombre)
+            if (mi_nombre == ''):
+                print('ingrese un nombre')
+            else:
+                print('el nombre es: '+mi_nombre.capitalize())
+                return (mi_nombre)
+        except TypeError:
+            print('No puede estar vacio el nombre')
+
+mi_persona = Persona('pAblO')
+print(mi_persona.nombrar('test'))
